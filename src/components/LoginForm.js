add1bd12efd80c86d8c13bdca9fa978e91269424
Nikola Pavlicek <npavlicek@users.noreply.github.com>
2023-09-15 13:58:21 -0400

    idk

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -2,11 +2,9 @@ function LoginForm() {
 	return (
 		<div class="h-screen w-screen flex items-center justify-center">
 			<form class="block w-1/4 h-1/4 shadow-xl space-y-5 text-center" method="POST" action="/auth/process">
-				<div class="flex items-center justify-center">
-					<input class="block w-1/2 text-center mx-auto shadow-md" type="text" name="username" required />
-					<input class="block w-1/2 text-center mx-auto shadow-md" type="password" name="password" required />
-					<input class="shadow-md p-5" type="submit" value="Login" />
-				</div>
+				<input class="block w-1/2 text-center mx-auto shadow-md" type="text" name="username" required />
+				<input class="block w-1/2 text-center mx-auto shadow-md" type="password" name="password" required />
+				<input class="shadow-md p-5" type="submit" value="Login" />
 			</form>
 		</div>
 	);
